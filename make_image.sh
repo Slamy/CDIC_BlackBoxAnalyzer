Slamy/Nobelia

Removal of tocsplit in favor of dd
Might work only because only a single track is used

make_image.sh
@@ -18,28 +18,11 @@ time SDL_VIDEODRIVER=dummy dosbox -conf dosbox.conf master.bat -exit
 cat build/MASTER.LOG
 grep "End   generation of album" build/MASTER.LOG
 
-# Convert the CDI/TOC files into CUE/BIN using mouse automation
-
-wine tocsplit.exe&
-sleep 0.3
-WID=$(xdotool search --name --sync tocsplit)
-xdotool windowactivate --sync $WID
-# Click "Open TOC"
-xdotool mousemove --sync -w $WID 220 340
-xdotool click -w $WID 1
-# Double-Click on first image
-sleep 0.3
-xdotool mousemove --sync -w $WID 40 40
-xdotool click --repeat 2 --delay 50 1
-# Click on GO!
-xdotool mousemove --sync -w $WID 320 340
-xdotool click -w $WID 1
-sleep 0.3
-# Click on Exit
-xdotool mousemove --sync -w $WID 500 340
-xdotool click -w $WID 1
-
-ls -lh disk/*.CUE disk/*.BIN
+# Convert the CDI/TOC files into CUE/BIN by cutting off the first 150 sectors
+dd skip=150 bs=2352 if=disk/NOBELIA.CDI of=disk/NOBELIA.BIN
+echo "FILE NOBELIA.BIN BINARY
+  TRACK 01 MODE2/2352
+    INDEX 01 00:00:00" > disk/NOBELIA.CUE
 
 sed -i -e "s/FILE .*\\\\/FILE /" disk/*.CUE
 

Slamy/CDIC_BlackBoxAnalyzer

make_image.sh
@@ -18,28 +18,11 @@ time SDL_VIDEODRIVER=dummy dosbox -conf dosbox.conf master.bat -exit
 cat build/MASTER.LOG
 grep "End   generation of album" build/MASTER.LOG
 
-# Convert the CDI/TOC files into CUE/BIN using mouse automation
-
-wine tocsplit.exe&
-sleep 0.3
-WID=$(xdotool search --name --sync tocsplit)
-xdotool windowactivate --sync $WID
-# Click "Open TOC"
-xdotool mousemove --sync -w $WID 220 340
-xdotool click -w $WID 1
-# Double-Click on first image
-sleep 0.3
-xdotool mousemove --sync -w $WID 40 40
-xdotool click --repeat 2 --delay 50 1
-# Click on GO!
-xdotool mousemove --sync -w $WID 320 340
-xdotool click -w $WID 1
-sleep 0.3
-# Click on Exit
-xdotool mousemove --sync -w $WID 500 340
-xdotool click -w $WID 1
-
-ls -lh disk/*.CUE disk/*.BIN
+# Convert the CDI/TOC files into CUE/BIN by cutting off the first 150 sectors
+dd skip=150 bs=2352 if=disk/CDICTEST.CDI of=disk/CDICTEST.BIN
+echo "FILE CDICTEST.BIN BINARY
+  TRACK 01 MODE2/2352
+    INDEX 01 00:00:00" > disk/CDICTEST.CUE
 
 sed -i -e "s/FILE .*\\\\/FILE /" disk/*.CUE
 